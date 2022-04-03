1b79d9c26c047f23dd58eb898802a27e07e9cc0d
crumwj22 <crumwj22@hotmail.com>
2022-04-02 22:19:46 -0700

    added API links to js, made two API calls, manipulated DOM for current weather, added moment.js and date to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
+// global variables
 var searchBtn  = document.querySelector('#searchBtn');
+var currentWeather = document.querySelector('.currentWeather');
+var fiveDayForecast = document.querySelector('.fiveDayForecast');
+var listSearchedCities = document.querySelector('.listSearchedCities');
 let pastSearches = JSON.parse(window.localStorage.getItem('pastSearches')) || [];
 
-
+//first API call
 searchBtn.addEventListener('click', function(event) {
     event.preventDefault();
     var cityName = document.querySelector('#cityInput').value;
-    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=130e45b8035df0b2f3d2389f4fb66852`
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a7715944e1c51c0dc7f0c45921d07581`
 
     fetch(requestUrl)
     .then(res => {
         return res.json()
+         
     })
     .then(data => {
         let lat = data.coord.lat;
@@ -19,28 +24,79 @@ searchBtn.addEventListener('click', function(event) {
         if(pastSearches.indexOf(cityName) === -1) {
             pastSearches.push(cityName);
             window.localStorage.setItem('pastSearches', JSON.stringify(pastSearches))
-            // Dynamically create List item from array
+       
         }
-
-        let oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=130e45b8035df0b2f3d2389f4fb66852&units=imperial`
+        // second API call
+        let oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&appid=a7715944e1c51c0dc7f0c45921d07581&units=imperial`
         fetch(oneCall)
         .then(response => {
            return response.json();
         })
         .then(oneCallData => {
             console.log(oneCallData);
-            // DOM manipulation to HTML;
+            renderDailyForecast(oneCallData) 
+                
         })
+    
+        // DOM manipulation to HTML;
+        // current weather DOM to HTML
+    function renderDailyForecast(oneCallData) {
+        const name = data.name;
+        const temp = oneCallData.current.temp;
+        const wind_speed = oneCallData.current.wind_speed;
+        const humidity = oneCallData.current.humidity;
+        const uvi = oneCallData.current.uvi;
+        const icon = oneCallData.current.weather[0].icon;
+        
+            
+        
+        
+        var nameEl = document.createElement('h1');
+        var tempEl = document.createElement('div');
+        var windEl = document.createElement('div');
+        var humidityEl = document.createElement('div');
+        var uvIndexEl = document.createElement('div');
+        var iconEl = document.createElement('img');
+        var iconDesEl = document.createElement('div');
+        iconEl.src = 'https://openweathermap.org/img/w/' + icon + '.png';
+        
+        var today = moment (); 
+        $(".currentWeather").text(today.format("MMM Do, YYYY"));
 
-        console.log(data)
+        currentWeather.append(nameEl, tempEl, windEl, humidityEl, uvIndexEl, iconEl, iconDesEl);
+        nameEl.textContent = "" + name;
+        tempEl.textContent = "Temp: " + temp;
+        windEl.textContent = "Wind Speed: " + wind_speed;
+        humidityEl.textContent = "Humidity: " + humidity;
+        uvIndexEl.textContent = "UV Index: " + uvi;
+
+            
+      }
+    
+    // create five day forecast cards
+    
+
+     })
+ })
+ // Dynamically create List item from array for city search history
+ function renderPastSearches() {
+    listSearchedCities.innerHTML = "";
+        for(let i = 0; i < pastSearches.length; i++) {
+        const historyItem = document.createElement("input");
+        historyItem.setAttribute("type","text");
+        historyItem.setAttribute("readonly",true);
+        historyItem.setAttribute("class", "form-control d-block bg-white");
+        historyItem.setAttribute("value", pastSearches[i]);
+        historyItem.addEventListener("click",function() {
+            renderPastSearches(historyItem.value);
     })
+    listSearchedCities.append(historyItem);
+    }  
+  }
 
-})
-
-for(let i = 0; i < pastSearches.length; i++) {
-    let h1 = document.createElement('h1');
-    h1.textContent = pastSearches[i];
-    document.querySelector('.listSearchedCities').appendChild(h1)
+renderPastSearches();
+if (pastSearches.length > 0) {
+renderPastSearches(pastSearches[pastSearches.length - 1]);
 }
-  
-  
+
+renderDailyForecast();
